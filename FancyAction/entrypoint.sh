27db22c563ffb0262758425d474cdede408f5b9d
monihl <119915878+monihl@users.noreply.github.com>
2024-07-17 19:15:55 +0530

    Update entrypoint.sh

--- FancyAction/entrypoint.sh
+++ FancyAction/entrypoint.sh
@@ -1,7 +1,5 @@
-#!/bin/sh -1
+#!/bin/sh -l
  
-echo "hi $1"
+echo "hello $1"
  
-echo "time is $(date)" >> $GITHUB_OUTPUT
- 
-echo "$(date) $1" >> $GITHUB_STEP_SUMMARY
+echo "time=$(date)" >> $GITHUB_OUTPUT
